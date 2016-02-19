b39c2e79ffda94ab654bb676ce4a4c3f8818f41d
Niels Holt <nho@fcoo.dk>
2016-02-19 16:16:50 +0100

    Release 1.0.0
    Released by Jesper Larsen jla@fcoo.dk 19-Feb-2016 16:16

---
+++ dist/leaflet-tilelayer-counting.js
@@ -0,0 +1,33 @@
+/****************************************************************************
+	leaflet-tilelayer-counting.js, 
+
+	(c) 2016, FCOO
+
+	https://github.com/FCOO/leaflet-tilelayer-counting
+	https://github.com/FCOO
+
+****************************************************************************/
+;(function (/*$,*/ L, window, document, undefined) {
+	"use strict";
+	L.TileLayer.Counting = L.TileLayer.extend({
+		initialize: function (url, options) {
+			this._counter = 0;
+			L.TileLayer.prototype.initialize.call(this, url, options);
+		},
+		addTo: function(map) {
+			if (this._counter === 0) {
+				L.TileLayer.prototype.addTo.call(this, map);
+			}
+			this._counter += 1;
+		},
+		removeFrom: function(map) {
+			if (this._counter == 1) {
+				L.TileLayer.prototype.removeFrom.call(this, map);
+			}
+			this._counter -= 1;
+		},
+	});
+}(/*jQuery,*/ L, this, document));
+
+
+
