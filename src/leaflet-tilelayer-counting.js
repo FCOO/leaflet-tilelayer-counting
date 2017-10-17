cb77495c05045d9c3d5a37b159520b106a110a20
Jesper Baasch-Larsen <jla@fcoo.dk>
2017-10-17 10:06:14 +0200

    added WMS layer

--- src/leaflet-tilelayer-counting.js
+++ src/leaflet-tilelayer-counting.js
@@ -27,6 +27,25 @@
             this._counter -= 1;
         },
     });
+
+    L.TileLayer.WMS.Counting = L.TileLayer.WMS.extend({
+        initialize: function (url, options) {
+            this._counter = 0;
+            L.TileLayer.WMS.prototype.initialize.call(this, url, options);
+        },
+        addTo: function(map) {
+            if (this._counter === 0) {
+                L.TileLayer.WMS.prototype.addTo.call(this, map);
+            }
+            this._counter += 1;
+        },
+        removeFrom: function(map) {
+            if (this._counter == 1) {
+                L.TileLayer.WMS.prototype.removeFrom.call(this, map);
+            }
+            this._counter -= 1;
+        },
+    });
 }(L, this, document));
 
 
